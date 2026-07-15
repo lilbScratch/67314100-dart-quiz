a676258cc86813c3439525d9d694bbb3dfe9cefd
Scratch <nicknamemyboom@gmail.com>
2026-07-15 11:58:02 +0700

    tam tow tee waiii

--- q2_oop.dart
+++ q2_oop.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 abstract class MenuItem{
   final String name;
   final double basePrice;
@@ -14,8 +16,24 @@ abstract class MenuItem{
 class Drink extends MenuItem {
   int topping;
   Drink(String name,double basePrice,this.topping) : super(name,basePrice);
-}
-
-void main(){
   
+  @override
+  double price() {
+    return basePrice + (10*topping);    
+  }
+}
+class Food extends MenuItem {
+  String size;
+  Food(String name,double basePrice,this.size) : super(name,basePrice);
+
+  @override
+  double price() {
+    if (size == 'S') {
+      return basePrice * 1;
+    } else if (size == 'M') {
+      return basePrice * 1.2;
+    } else if (size == 'L') {
+      return basePrice * 1.5;
+    }
+  }
 }
